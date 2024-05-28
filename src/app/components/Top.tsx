f7b83f15840507f711f6cd9f896e9172bcb4ea35
Luckyxonepeice <lakshaychauhan129@gmail.com>
2024-05-28 17:19:25 +0530

    Added Top ANIME and MANGA Section

--- src/app/components/Top.tsx
+++ src/app/components/Top.tsx
@@ -1,10 +1,6 @@
 'use client'
 
 import React, { useEffect, useState } from 'react'
-import Image from 'next/image'
-import logo from "../../../public/images/logo.jpg"
-
-
 
 const data = {
     images:{
@@ -13,7 +9,7 @@ const data = {
     title:"Dragon Ball Z"
 }
 
-export default function Top() {
+export default function Top({url}:{url:any}) {
 
     const [topData, setTopData]= useState([]);
     useEffect(()=>{        
@@ -27,13 +23,12 @@ export default function Top() {
     },[])
 
     async function fetchData(){
-
-        const result = await fetch('http://localhost:3000/api/details',{cache:'force-cache'});
-        return await result.json();
+        const result = await fetch(url);
+        const val = await result.json();
+        return val['data'];
     }
   return (
       <div className='w-3/4 h-full flex flex-row gap-6 overflow-x-auto flex-nowrap flex-none snap-mandatory snap-x mx-auto mt-10'>
-
         {
             topData.map( (val:any)=>{
                 return <Card key={val.mal_id} data={val}/>
@@ -47,10 +42,10 @@ export default function Top() {
 
 function Card({data}:{data:any}){
 
-    console.log(data);
-    return <div className='h-60 min-w-52 bg-black snap-center '>
-        <Image src={logo} alt='Anime Front Image'></Image>
-        <h1 className='text-white'>{data.title}</h1>
+    const img_loc = data["images"]["jpg"]["image_url"];
+    return <div className='h-60 min-w-52 bg-black snap-center mb-0 hover:drop-shadow-md'>
+            <img className="h-3/4 w-full" src={img_loc} alt='Anime Front Image'></img>
+            <h1 className='text-white text-center text-sm'>{data.title}</h1>
     </div>
 }
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import Top from './components/Top';
+import './home.css'
  
 export const metadata: Metadata = {
   title: 'AniWorld',
@@ -9,7 +10,10 @@ export const metadata: Metadata = {
 export default function Home() {
   return (
     <div>
-      <Top/>
+      <h1 className='text-3xl text-white text-center mt-10'>TOP ANIME</h1>
+      <Top url={'https://api.jikan.moe/v4/top/anime'}/>
+      <h1 className='text-3xl text-white text-center mt-10'>TOP MANGA</h1>
+      <Top url={'https://api.jikan.moe/v4/top/manga'}/>
     </div>
   );
 }
